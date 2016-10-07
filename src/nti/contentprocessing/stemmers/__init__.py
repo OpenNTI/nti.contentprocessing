a5d2ad4306cbd7d07716c61fd2abe44c1285b443
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-10-07 16:08:34 +0000

    minor
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentprocessing/trunk@98347 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentprocessing/stemmers/__init__.py
+++ src/nti/contentprocessing/stemmers/__init__.py
@@ -14,12 +14,13 @@ logger = __import__('logging').getLogger(__name__)
 import repoze.lru
 
 from zope import component
-from zope import interface
 
 from nti.contentprocessing.stemmers.interfaces import IStemmer
 
 @repoze.lru.lru_cache(200)
 def stem_word(word, lang='en', name=''):
-	stemmer = component.getUtility(IStemmer, name=name)
-	result = stemmer.stem(word, lang) if word else None
-	return result
+	stemmer = component.queryUtility(IStemmer, name=name)
+	if stemmer is not None:
+		return stemmer.stem(word, lang) if word else None
+	return word
+stem = stem_word
