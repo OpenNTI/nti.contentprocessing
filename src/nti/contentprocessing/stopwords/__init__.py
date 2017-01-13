975cc153c72a6e73326c529e48fc3566a3ff5a1e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-13 03:57:25 +0000

    add keywords from solr
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentprocessing/trunk@104258 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentprocessing/stopwords/__init__.py
+++ src/nti/contentprocessing/stopwords/__init__.py
@@ -18,40 +18,40 @@ from nti.contentprocessing.interfaces import IStopWords
 
 from nti.property.property import Lazy
 
+
 @interface.implementer(IStopWords)
 class FileBasedStopWords(object):
 
-	@Lazy
-	def _cache(self):
-		result = {}
-		path = os.path.join(os.path.dirname(__file__), 'data')
-		for name in os.listdir(path):
-			if not name.endswith('.txt'):
-				continue
-			lang = name[:-4]
-			name = os.path.join(path, name)
-			with codecs.open(name, "r", "utf-8") as fp:
-				lines = {x.strip().lower() for x in fp.readlines()
-						 if x and not x.startswith('#')}
-				result[lang] = tuple(sorted(lines))
-		return result
+    @Lazy
+    def _cache(self):
+        result = {}
+        path = os.path.join(os.path.dirname(__file__), 'data')
+        for name in os.listdir(path):
+            if not name.endswith('.txt'):
+                continue
+            lang = name[:-4]
+            name = os.path.join(path, name)
+            with codecs.open(name, "r", "utf-8") as fp:
+                lines = {x.strip().lower() for x in fp.readlines()
+                         if x and not x.startswith('#')}
+                result[lang] = tuple(sorted(lines))
+        return result
 
-	def stopwords(self, lang='en'):
-		return self._cache.get(lang, ())
+    def stopwords(self, lang='en'):
+        return self._cache.get(lang, ())
 
-	def available_languages(self):
-		return tuple(sorted(self._cache.keys()))
-_FileBasedStopWords = FileBasedStopWords
+    def available_languages(self):
+        return tuple(sorted(self._cache.keys()))
+_DefaultStopWords = _FileBasedStopWords = FileBasedStopWords
+
 
 @interface.implementer(IStopWords)
 class NoStopWords(object):
 
-	__slots__ = ()
+    __slots__ = ()
 
-	def stopwords(self, lang='en'):
-		return ()
+    def stopwords(self, lang='en'):
+        return ()
 
-	def available_languages(self,):
-		return ('en', 'es', 'ru')
-
-_DefaultStopWords = _NoStopWords = NoStopWords
+    def available_languages(self,):
+        return ('en', 'es', 'ru')
