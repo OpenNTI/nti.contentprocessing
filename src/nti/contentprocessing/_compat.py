99abaf5519f3041dbaa6893a88e66d0730e59f50
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-04-07 22:55:58 +0000

    copy from pyramid
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentprocessing/trunk@110402 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentprocessing/_compat.py
+++ src/nti/contentprocessing/_compat.py
@@ -40,6 +40,24 @@ def unicode_(s, encoding='utf-8', err='strict'):
     """
     s = s.decode(encoding, err) if isinstance(s, bytes) else s
     return _unicode(s) if s is not None else None
-
-
 to_unicode = unicode_
+
+
+if PY3:  # pragma: no cover
+    def native_(s, encoding='latin-1', errors='strict'):
+        """
+        If ``s`` is an instance of ``text_type``, return
+        ``s``, otherwise return ``str(s, encoding, errors)``
+        """
+        if isinstance(s, text_type):
+            return s
+        return str(s, encoding, errors)
+else:
+    def native_(s, encoding='latin-1', errors='strict'):
+        """
+        If ``s`` is an instance of ``text_type``, return
+        ``s.encode(encoding, errors)``, otherwise return ``str(s)``
+        """
+        if isinstance(s, text_type):
+            return s.encode(encoding, errors)
+        return str(s)
