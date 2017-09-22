9bc4687f85972ece8551630f0033b2e924cea7e3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-09-22 00:15:35 +0000

    pylint
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentprocessing/trunk@122092 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentprocessing/_compat.py
+++ src/nti/contentprocessing/_compat.py
@@ -4,17 +4,11 @@
 .. $Id$
 """
 
-from __future__ import print_function, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-logger = __import__('logging').getLogger(__name__)
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 import six
-
-if six.PY3:  # pragma: no cover
-    def _unicode(s): return str(s)
-else:
-    _unicode = unicode
 
 
 def text_(s, encoding='utf-8', err='strict'):
@@ -22,5 +16,5 @@ def text_(s, encoding='utf-8', err='strict'):
     Decode a byte sequence and unicode result
     """
     s = s.decode(encoding, err) if isinstance(s, bytes) else s
-    return _unicode(s) if s is not None else None
+    return six.text_type(s) if s is not None else None
 unicode_ = to_unicode = text_
