5bf7640973f4b526f1576556dc8a531c7300c342
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-09-27 17:07:54 +0000

    document and remove aliases
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentprocessing/trunk@122405 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentprocessing/_compat.py
+++ src/nti/contentprocessing/_compat.py
@@ -13,8 +13,12 @@ import six
 
 def text_(s, encoding='utf-8', err='strict'):
     """
-    Decode a byte sequence and unicode result
+    Return a string and unicode version of an object. 
+    If the object is an byte sequence it's decoded first
+    
+    :param object s: The object to get an unicode representation of.
+    :param str encoding: The encoding to be used if ``s`` is a byte sequence
+    :param str err: The err handling scheme to be used if ``s`` is a byte sequence
     """
     s = s.decode(encoding, err) if isinstance(s, bytes) else s
     return six.text_type(s) if s is not None else None
-unicode_ = to_unicode = text_
