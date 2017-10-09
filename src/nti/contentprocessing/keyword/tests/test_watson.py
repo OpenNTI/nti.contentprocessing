3ff76a34b9fdfcbb5ee5778a1c7eb2d285b40adc
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-10-09 19:27:58 +0000

    92%
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentprocessing/trunk@122927 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentprocessing/keyword/tests/test_watson.py
+++ src/nti/contentprocessing/keyword/tests/test_watson.py
@@ -7,6 +7,8 @@ __docformat__ = "restructuredtext en"
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
+from hamcrest import is_
+from hamcrest import none
 from hamcrest import has_key
 from hamcrest import has_length
 from hamcrest import assert_that
@@ -16,9 +18,14 @@ import fudge
 import os
 import unittest
 
+from zope import component
+
 import simplejson
 
-from nti.contentprocessing.keyword import watson
+from nti.contentprocessing.keyword.interfaces import IKeyWordExtractor
+
+from nti.contentprocessing.keyword.watson import analyze
+from nti.contentprocessing.keyword.watson import get_keywords
 
 from nti.contentprocessing.tests import SharedConfiguringTestLayer
 
@@ -41,9 +48,24 @@ class TestConceptTagger(unittest.TestCase):
     @fudge.patch('nti.contentprocessing.keyword.watson.analyze')
     def test_watson_keywords(self, mock_an):
         mock_an.is_callable().with_args().returns(self.response)
-        terms = watson.get_keywords(self.sample)
+        terms = get_keywords(self.sample)
         terms = {r.token: r.relevance for r in terms}
         assert_that(terms, has_length(20))
         assert_that(terms, has_key('knobby green objects'))
         assert_that(terms, has_key('blood cells'))
         assert_that(terms, has_key('red blood cells'))
+
+    def test_analyze(self):
+        client = fudge.Fake().provides('analyze').returns(None)
+        assert_that(analyze(client, ''), is_(none()))
+        
+    @fudge.patch('nti.contentprocessing.keyword.watson.analyze')
+    def test_coverage(self, mock_grc):
+        mock_grc.is_callable().with_args().returns(self.response)
+        extractor = component.getUtility(IKeyWordExtractor, name="watson")
+        terms = extractor([self.sample])
+        assert_that(terms, has_length(20))
+
+        mock_grc.is_callable().raises(TypeError)
+        terms = get_keywords(self.sample)
+        assert_that(terms, is_(()))
