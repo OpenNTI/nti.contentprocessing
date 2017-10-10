5e7172e0df466c49dfdb873bce288941261d29b8
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-10-10 03:17:05 +0000

    pylint
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentprocessing/trunk@122941 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentprocessing/monkey.py
+++ src/nti/contentprocessing/monkey.py
@@ -4,10 +4,9 @@
 .. $Id$
 """
 
-from __future__ import print_function, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-logger = __import__('logging').getLogger(__name__)
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 # All the patching uses private things so turn that warning off
 # pylint: disable=W0212
