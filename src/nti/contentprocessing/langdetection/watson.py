6085e167d13a546b5cdda28da7ec3f8cde4355f9
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-10-10 03:40:33 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentprocessing/trunk@122946 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentprocessing/langdetection/watson.py
+++ src/nti/contentprocessing/langdetection/watson.py
@@ -6,10 +6,9 @@ Watson lang detector
 .. $Id$
 """
 
-from __future__ import print_function, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-logger = __import__('logging').getLogger(__name__)
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 from zope import interface
 
@@ -27,6 +26,8 @@ from nti.contentprocessing.watson import get_language_translator_client
 from nti.property.property import alias
 
 from nti.schema.fieldproperty import createDirectFieldProperties
+
+logger = __import__('logging').getLogger(__name__)
 
 
 @WithRepr
