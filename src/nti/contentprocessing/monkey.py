8cea74a09dba90c5d06c793cfe3c10c6d0bfebe8
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-06-29 10:35:41 -0500

    don't fail if html5lib cannot be patched

--- src/nti/contentprocessing/monkey.py
+++ src/nti/contentprocessing/monkey.py
@@ -11,18 +11,23 @@ from __future__ import absolute_import
 # pylint: disable=protected-access,non-parent-init-called
 
 def _patch_html5lib():
-    import html5lib
-    from html5lib._inputstream import HTMLBinaryInputStream
-
-    class _HTMLBinaryInputStream(HTMLBinaryInputStream):
-
-        def __init__(self, *args, **kwargs):
-            if 'encoding' in kwargs:
-                encoding = kwargs.pop('encoding', None)
-                kwargs['override_encoding'] = encoding
-            HTMLBinaryInputStream.__init__(self, *args, **kwargs)
-
-    html5lib._inputstream.HTMLBinaryInputStream = _HTMLBinaryInputStream
+    try:
+        import html5lib
+        from html5lib._inputstream import HTMLBinaryInputStream
+    
+        class _HTMLBinaryInputStream(HTMLBinaryInputStream):
+    
+            def __init__(self, *args, **kwargs):
+                if 'encoding' in kwargs:
+                    encoding = kwargs.pop('encoding', None)
+                    kwargs['override_encoding'] = encoding
+                HTMLBinaryInputStream.__init__(self, *args, **kwargs)
+    
+        html5lib._inputstream.HTMLBinaryInputStream = _HTMLBinaryInputStream
+    except ImportError:
+        import warnings
+        warnings.warn('Cannot patch html5lib HTMLBinaryInputStream',
+                      DeprecationWarning, stacklevel=1)
 _patch_html5lib()
 del _patch_html5lib
 
