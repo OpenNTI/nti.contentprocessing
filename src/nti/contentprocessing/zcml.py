1c74731f5213e199324d7dce9c75c6352c2f60b2
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-09-26 16:31:47 -0500

    use zope.schema

--- src/nti/contentprocessing/zcml.py
+++ src/nti/contentprocessing/zcml.py
@@ -8,13 +8,15 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
+# pylint: disable=inherit-non-class
+
 from functools import partial
 
 from zope import interface
 
 from zope.component.zcml import utility
 
-from zope.configuration import fields
+from zope.schema import TextLine
 
 from nti.contentprocessing._compat import text_
 
@@ -22,24 +24,26 @@ from nti.contentprocessing.interfaces import IWatsonAPIKey
 
 from nti.contentprocessing.watson import create_api_key
 
+logger = __import__('logging').getLogger(__name__)
+
 
 class IRegisterWatsonAPIKeyDirective(interface.Interface):
     """
     The arguments needed for registering a key
     """
-    name = fields.TextLine(title=u"The name",
-                           required=False)
+    name = TextLine(title=u"The name",
+                    required=False)
 
-    username = fields.TextLine(title=u"The username", required=True)
+    username = TextLine(title=u"The username", required=True)
 
-    password = fields.TextLine(title=u"The password", required=True)
+    password = TextLine(title=u"The password", required=True)
 
 
 def registerWatsonAPIKey(_context, username, password, name=''):
     """
     Register an watson key with the given alias
     """
-    factory = partial(create_api_key, 
-                      username=text_(username), 
+    factory = partial(create_api_key,
+                      username=text_(username),
                       password=text_(password))
     utility(_context, provides=IWatsonAPIKey, factory=factory, name=name)
